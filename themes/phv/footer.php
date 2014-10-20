8643e1efd4fc0770e5f47b4f2c3def5161700c94
Viet Pham <phamhongviet52@gmail.com>
2014-10-20 23:01:49 +0700

    theme: phv

---
+++ themes/phv/footer.php
@@ -0,0 +1,10 @@
+		<div class="wrap">
+	            <footer id="bottom">
+	                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
+
+	            </footer>
+
+	        </div>
+        </div>
+    </body>
+</html>
